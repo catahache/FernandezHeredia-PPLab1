17845f77db863f676be91be0c3a960810dbf5228
Cata Hache <herediacatalina1995@gmail.com>
2020-05-13 08:24:11 -0300

    primer commit

---
+++ primerParcial/src/primerParcial.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : primerParcial.c
+ Author      : Catalina Fernandez Heredia
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+	return EXIT_SUCCESS;
+}
